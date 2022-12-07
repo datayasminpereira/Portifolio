d431b82534fd1683f133c8353af3093a3080bbd3
Yasmin Pereira <yasminbarroso0208@gmail.com>
2022-12-07 16:37:21 -0300

    Home mobile: falta botao saberMais abrir o menu

--- scripts.js
+++ scripts.js
@@ -4,9 +4,6 @@ const menu = document.querySelector(".menu-lista-mobile")
 
 menuBotao.addEventListener("click", () => {
     menu.classList.toggle("menu-lista-mobile--ativo")
-    console.log(menu.classList)
-    menu.id = "inserindo"
-    console.log(menu.id)
 })
 
 
@@ -23,13 +20,31 @@ document.querySelectorAll('.outro-html').forEach(link => {
 }
 })
 
-// BOTAO SABERMAIS: ao ser clicado fazer animacao nos links do menu
+
+// AINDA NAO RESOLVIDO
+
+// BOTAO SABERMAIS DESKTOP: ao ser clicado fazer animacao nos links do menu
 
 const botaoSaberMais = document.querySelector(".saber-mais")
 const linksMenu = document.querySelectorAll(".menu-lista__link")
+const nav = document.querySelector(".menu")
 
-botaoSaberMais.addEventListener("click", () => {
-    linksMenu.forEach(link => {
-        link.classList.toggle("menu-lista__link-animacao")
+console.log(nav)
+console.log(getComputedStyle(nav).height)
+console.log(getComputedStyle(nav).height == "33px")
+if (getComputedStyle(nav).height != "33px") { // menu desktop
+
+    botaoSaberMais.addEventListener("click", () => {
+        linksMenu.forEach(link => {
+            link.classList.toggle("menu-lista__link-animacao")
+        })
     })
-})
+} else { // menu mobile
+
+    botaoSaberMais.addEventListener("click", () => {
+
+        menu.classList.toggle("menu-lista-mobile--ativo")
+
+    })
+
+}
